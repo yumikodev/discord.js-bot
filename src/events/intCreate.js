1c389afc91ce6890494de370e9cb589e94a280ed
Yasu Yumiko <yumikokun0828@gmail.com>
2022-07-12 16:25:12 -0500

    estructura actualizada

--- src/events/intCreate.js
+++ src/events/intCreate.js
@@ -1,25 +1,26 @@
-const client = require("../../index");
-
 /* Interaction create */
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
+module.exports = {
+  name: "run",
+  async run(client, interaction) {
+    if (!interaction.isCommand()) return;
 
-  const command = client.slashs.get(interaction.commandName);
+    const command = client.slashs.get(interaction.commandName);
 
-  if (!command)
-    return interaction.reply({
-      content: "An error has ocurred",
-      ephemeral: true,
-    });
+    if (!command)
+      return interaction.reply({
+        content: "An error has ocurred",
+        ephemeral: true,
+      });
 
-  try {
-    await command.execute(client, interaction);
-  } catch (err) {
-    console.error(err);
+    try {
+      await command.execute(client, interaction);
+    } catch (err) {
+      console.error(err);
 
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  },
+};
