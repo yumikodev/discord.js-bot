1ed501412022bda07191e80b6d97a510fce2d979
Yasu Yumiko <yumikokun0828@gmail.com>
2022-07-12 16:26:21 -0500

    handler actualizado

--- src/handler/index.js
+++ src/handler/index.js
@@ -1,34 +1,38 @@
-const path = require("path");
-const fs = require("fs");
+const path = require("node:path");
+const fs = require("node:fs");
 
 module.exports = async (client) => {
-  /*--------- Command handler ---------*/
-  const commandFiles = fs
-    .readdirSync(path.join(__dirname, "../commands/cmd"))
-    .filter((file) => file.endsWith(".js"));
+  try {
+    /*--------- Command handler ---------*/
+    const commandFiles = fs
+      .readdirSync(path.join(__dirname, "../commands/cmd"))
+      .filter((file) => file.endsWith(".js"));
 
-  for (const file of commandFiles) {
-    const command = require(`../commands/cmd/${file}`);
-    client.commands.set(command.name, command);
-  }
+    for (const file of commandFiles) {
+      const command = require(`../commands/cmd/${file}`);
+      await client.commands.set(command.name, command);
+    }
 
-  /*--------- slash handler ---------*/
-  const slashFiles = fs
-    .readdirSync(path.join(__dirname, "../commands/slash"))
-    .filter((file) => file.endsWith(".js"));
+    /*--------- slash handler ---------*/
+    const slashFiles = fs
+      .readdirSync(path.join(__dirname, "../commands/slash"))
+      .filter((file) => file.endsWith(".js"));
 
-  for (const file of slashFiles) {
-    const command = require(`../commands/slash/${file}`);
-    // console.log(command);
-    client.slashs.set(command.data.name, command);
-  }
+    for (const file of slashFiles) {
+      const command = require(`../commands/slash/${file}`);
+      await client.slashs.set(command.data.name, command);
+    }
 
-  /*------------- Event handler -------------*/
-  const eventFiles = fs
-    .readdirSync(path.join(__dirname, "../events"))
-    .filter((file) => file.endsWith(".js"));
+    /*------------- Event handler -------------*/
+    const eventFiles = fs
+      .readdirSync(path.join(__dirname, "../events"))
+      .filter((file) => file.endsWith(".js"));
 
-  for (const file of eventFiles) {
-    require(`../events/${file}`);
+    for (const file of eventFiles) {
+      const e = require(`../events/${file}`);
+      await client.on(e.name, (...args) => e.run(client, ...args));
+    }
+  } catch (err) {
+    console.error(err);
   }
 };
